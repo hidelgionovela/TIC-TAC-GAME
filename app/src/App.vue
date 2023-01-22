088db528c89ac29b767ea81daec8c594c7d1a44a
Hidélgio Novela <hidelgionovela@gmail.com>
2023-01-22 16:00:44 +0200

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -52,18 +52,36 @@ const ResetGame = () => {
 </script>
 
 <template>
-  <main class="pt-8 text-center dark:bg-gray-800 min-h-screen dark:text-white">
+  <!-- <main class="pt-8 text-center dark:bg-gray-800 min-h-screen dark:text-white"> -->
+  <main class="pt-8 text-center bg-gray-800 min-h-screen text-white">
 
-    <h1 class="mb-8 text-3x1 font-bold uppercase"> Tic Tac Game</h1>
+    <h1 class="mb-8 text-3xl font-bold uppercase"> Tic Tac Game </h1>
 
     <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
 
     <div class="flex flex-col items-center mb-8">
-      <div>
-        <!-- v-for -->
+      <div v-for ="(row,x) in board"
+      :key ="x"
+      class="flex">
+        <div v-for="(cell,y) in row" 
+        :key="y"
+        @click="MakeMove(x,y)"
+        :class="`border border-white w-20 h-20 hover:bg-gray-700 flex
+        items-center justify-center material-icons-outlined text-4xl
+        cursor-pointer ${ cell === 'X' ? 'text-red-500' : 'text-green-500'}`">
+
+        {{ cell === "X" ? 'close' : cell === "O" ? 'circle' : "" }}
+
+        </div>
+
       </div>
 
     </div>
+
+    
+    <h2 v-if="winner" class="text-6xl fond-bold mb-8"> Player '{{winner}}' Wins!</h2>
+
+    <button @click="ResetGame" id="rest">Reset</button>
   </main>
 </template>
 
